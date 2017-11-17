d8f8e58ef8458fa7a1d862cf2f9af0d12f4f2f3e
Benjamin Lasseur <benjamin.lasseur@gmail.com>
2017-11-17 16:57:49 +0100

    Changement score coté serveur

--- src/main/java/com/example/topcolleguesbackend/controller/CollegueController.java
+++ src/main/java/com/example/topcolleguesbackend/controller/CollegueController.java
@@ -1,9 +1,11 @@
 package com.example.topcolleguesbackend.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +27,9 @@ public class CollegueController {
 	@Autowired
 	CollegueRepository colRep;
 
-	/**Méthode get de listage des collegues
+	/**
+	 * Méthode get de listage des collegues
+	 * 
 	 * @return
 	 */
 	@GetMapping("")
@@ -33,7 +37,14 @@ public class CollegueController {
 		return this.colRep.findAll();
 	}
 
-	/**Méthode post d'ajout de collegue
+	@GetMapping("/{id}/detail")
+	public Collegue getBanqueCollaborateur(@PathVariable Integer id) {
+		return this.colRep.findOne(id);
+	}
+
+	/**
+	 * Méthode post d'ajout de collegue
+	 * 
 	 * @param col
 	 * @return
 	 */
@@ -45,17 +56,28 @@ public class CollegueController {
 			this.colRep.save(col);
 		}
 		return this.colRep.findOne(col.getId());
+
 	}
 
-	/**Méthode put de modification du score
+	/**
+	 * Méthode put de modification du score
+	 * 
 	 * @param col
 	 * @param id
 	 * @return
 	 */
 	@PutMapping("/{id}/score")
-	public Collegue update(@RequestBody Collegue col, @PathVariable int id) {
+	public Collegue update(@RequestBody Map<String,String> avis, @PathVariable int id) {
 		Collegue oldCol = this.colRep.findOne(id);
-		oldCol.setScore(col.getScore());
+		switch (avis.get("avis")) {
+		case "aimer":
+			oldCol.setScore(oldCol.getScore() + 10);
+			break;
+		case "detester":
+			oldCol.setScore(oldCol.getScore() - 5);
+			break;
+		default:
+		}
 		this.colRep.save(oldCol);
 		return this.colRep.findOne(id);
 	}
